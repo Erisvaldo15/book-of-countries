b17ead1f6f7285f78d188ff69e71ea2b5b054fcf
Erisvaldo <erisvaldosilvadesousa38@gmail.com>
2022-07-23 12:30:00 -0300

    first-version-updated

--- src/exports/createElements.js
+++
@@ -1,7 +0,0 @@
-export const country = document.createElement('div');
-export const header = document.createElement('header');
-export const body = document.createElement('main');
-export const image = document.createElement('img');
-export const title = document.createElement('h3');
-export const description = document.createElement('p');
-export const anchor = document.createElement('a');
--- src/exports/render.js
+++ src/exports/render.js
@@ -1,29 +1,35 @@
 import { section } from './selectors';
 import { stringToArray } from './formatters';
-import * as element from './createElements';
 
 export default function render(countries) {
+    
     countries.forEach((prop) => {
+
+        const country = document.createElement('div');
+        const header = document.createElement('header');
+        const body = document.createElement('main');
+        const image = document.createElement('img');
+        const title = document.createElement('h3');
+        const description = document.createElement('p');
+        const anchor = document.createElement('a');
 
         const formatted = stringToArray(`${prop.image}`, '.');
 
-        console.log(element.country)
+            section.appendChild(country).setAttribute('id', 'country');
 
-        section.appendChild(element.country).setAttribute('id', 'country');
+            country.appendChild(header);
+            country.appendChild(body);
+            header.appendChild(image);
+            header.appendChild(anchor);
+            image.src = `assets/img/${prop.image}`;
 
-        element.country.appendChild(element.header);
-        element.country.appendChild(element.body);
-        element.header.appendChild(element.image);
-        element.header.appendChild(element.anchor);
-        element.image.src = `assets/img/${prop.image}`;
+            body.appendChild(title).textContent = prop.name;
+            body.appendChild(description).textContent = prop.description;
+            body.appendChild(anchor).textContent = 'View more';
 
-        element.body.appendChild(element.title).textContent = prop.name;
-        element.body.appendChild(element.description).textContent = prop.description;
-        element.body.appendChild(element.anchor).textContent = 'View more';
-
-        element.anchor.setAttribute(
-            'href',
-            `views/country.html?country=${formatted[0]}`
-        );
+            anchor.setAttribute(
+                'href',
+                `views/country.html?country=${formatted[0]}`
+            );
     });
 }
